52c3af0fce44d8a1083cfac9285f0ce32f0de5a1
emoryhubbardbyui <hub21003@byui.edu>
2022-09-18 03:16:54 -0400

    Fixed JS and made folders

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,13 +3,19 @@ let year = date.getFullYear();
 
 document.getElementById("copyright-line").innerHTML = `&copy ${year} &#124 Emory D. Hubbard &#124 Maryland`;
 
+function padWithZeroes(number, zeroQuantity) {
+  return number.toString().padStart(zeroQuantity, '0');
+}
+
 let lastModified = new Date(document.lastModified);
-let modDay = lastModified.getDay();
-let modMonth = lastModified.getMonth();
+let modDay = lastModified.getDate();
+let modMonth = lastModified.getMonth() + 1;
 let modYear = lastModified.getFullYear();
-let modHour = lastModified.getHours();
-let modMinute = lastModified.getMinutes();
-let modSecond = lastModified.getSeconds();
+let modHour = padWithZeroes(lastModified.getHours(), 2);
+let modMinute = padWithZeroes(lastModified.getMinutes(), 2);
+let modSecond = padWithZeroes(lastModified.getSeconds(), 2);
 
 document.getElementById("update-line").innerHTML =
-  `Last Updated: ${modMonth}/${modDay}/${modYear} ${modHour}:${modMinute}:${modSecond}`
+  `Last Updated: ${modMonth}/${modDay}/${modYear} ${modHour}:${modMinute}:${modSecond}`;
+
+ 
