ea31a006facd56051c07bbe3ef7078b058494ac6
emoryhubbardbyui <113547542+emoryhubbardbyui@users.noreply.github.com>
2022-10-29 00:20:12 -0400

    Uploaded last week's assignment

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,21 @@
+main();
+
+function main() {
+    const temp = parseFloat(document.querySelector("#tvalue").innerHTML);
+    const speed = parseFloat(document.querySelector("#svalue").innerHTML);
+    const chillElement = document.querySelector("#cvalue");
+
+    chillElement.innerHTML =getChill(temp, speed);
+}
+
+function getChill(temp, speed) {
+    let chill = "N/A";
+
+    if (temp <= 50 && speed > 3) {
+        value = 35.74 + 0.6215 * temp - 32.75 * Math.pow(speed, 0.16)
+            + 0.4275 * temp * Math.pow(speed, 0.16);
+        chill = `${Math.round(value)}°F`
+    }
+
+    return chill;
+}
