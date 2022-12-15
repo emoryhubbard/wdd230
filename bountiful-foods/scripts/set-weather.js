7ce105809d2fd0f16d7aa70c18d0e202747f8e8b
emoryhubbardbyui <hub21003@byui.edu>
2022-12-15 16:21:30 -0500

    Added weather forecast

--- bountiful-foods/scripts/set-weather.js
+++ bountiful-foods/scripts/set-weather.js
@@ -19,6 +19,20 @@ async function setWeather() {
     } catch (error) {
       console.log(error);
     }
+    
+    const url2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`;
+    try {
+        const response = await fetch(url2);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          displayForecast(data);
+        } else {
+          throw Error(await response.text());
+        }
+      } catch (error) {
+        console.log(error);
+      }
  }
   
 function displayResults(data) {
@@ -50,4 +64,24 @@ function capitalize(word) {
     const firstLetter = word.charAt(0).toUpperCase();
     const restOfWord = word.slice(1);
     return firstLetter + restOfWord;
+}
+
+function displayForecast(data) {
+  const tempList = data["list"];
+  console.log(tempList);
+
+  let day1 = tempList[8].main.temp;
+  console.log(day1);
+  console.log(getFahr(day1));
+
+  day1 = Math.round(getFahr(day1));
+  const day2 = Math.round(getFahr(tempList[16].main.temp));
+  const day3 = Math.round(getFahr(tempList[24].main.temp));
+
+  document.querySelector(".day1").innerHTML = day1;
+  document.querySelector(".day2").innerHTML = day2;
+  document.querySelector(".day3").innerHTML = day3;
+}
+function getFahr(kel) {
+  return 1.8 * (kel - 273) + 32;
 }
